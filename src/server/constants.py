d9decd4e4739093145f3aa37213fee234c7dbc2f
Mike K <mikecknatz@gmail.com>
2020-03-27 20:10:00 -0400

    20200327: added hallways to constants file

--- src/server/constants.py
+++ src/server/constants.py
@@ -40,3 +40,23 @@ BALLROOM="Ballroom"
 KITCHEN="Kitchen"
 
 ROOMS=[STUDY,HALL,LOUNGE,LIBRARY,BILLIARD,DINING,CONSERVATORY,BALLROOM,KITCHEN]
+
+#Hallways - Treated as rooms for movement, but no actual guesses may be made.
+#No hallways for secret passages, suggest having an array of connections in each
+#room object and referencing our constants for what it is.
+STUDY_HALL="Study-Hall Hallway"
+HALL_LOUNGE="Hall-Lounge Hallway"
+STUDY_LIBRARY="Study-Library Hallway"
+HALL_BILLIARD="Hall-Billiard Room Hallway"
+LOUNGE_DINING="Lounge-Dining Room Hallway"
+LIBRARY_BILLIARD="Library-Billiard Room Hallway"
+BILLIARD_DINING="Billiard Room-Dining Room Hallway"
+LIBRARY_CONSERVATORY="Library-Conservatory Hallway"
+BILLIARD_BALLROOM="Billiard Room-Ballroom Hallway"
+DINING_KITCHEN="Dining Room-Kitchen Hallway"
+CONSERVATORY_BALLROOM="Conservatory-Ballroom Hallway"
+BALLROOM_KITCHEN="Ballroom-Kitchen Hallway"
+
+HALLWAYS=[STUDY_HALL, HALL_LOUNGE, STUDY_LIBRARY, HALL_BILLIARD, LOUNGE_DINING, LIBRARY_BILLIARD,
+BILLIARD_DINING, LIBRARY_CONSERVATORY, BILLIARD_BALLROOM, DINING_KITCHEN, CONSERVATORY_BALLROOM,
+BALLROOM_KITCHEN]
