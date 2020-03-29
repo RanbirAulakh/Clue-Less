a8729b86c5092cff8ff454f214a1eacfe7a07f86
Mike K <mikecknatz@gmail.com>
2020-03-29 13:53:42 -0400

    20200329: Filled out game class, minor testing within the class

--- src/server/game.py
+++ src/server/game.py
@@ -1,34 +1,75 @@
 import constants
 import random
-import player
+from player import Player
 import room
-import clue
+from clue import Clue
 import map
 
 class Game():
 	def __init__(self, id, players):
 		#create decks of the different clue types
-		suspects = createSuspectDeck(constants.SUSPECTS)
-		weapons = createWeaponsDeck(constants.WEAPONS)
-		rooms = createRoomsDeck(constants.ROOMS)
+		suspects = self.createSuspectDeck(constants.SUSPECTS)
+		weapons = self.createWeaponsDeck(constants.WEAPONS)
+		rooms = self.createRoomsDeck(constants.ROOMS)
 		# draw 1 from each and store it as the murder you are solving
-		storedMurder = createMurder(suspects, rooms, weapons)
+		storedMurder = self.createMurder(suspects, rooms, weapons)
 		#combine the decks then deal to the players)
-		clueDeck = random.shuffle(suspects + weapons + rooms)
+		clueDeck = suspects + weapons + rooms
+		random.shuffle(clueDeck)
+		numPlayers = len(players)
+		self.dealHands(players,clueDeck)
 		pass
 		
-	def createSuspectDeck(suspects):
+	# create individual decks for the creation of the muder
+	def createSuspectDeck(self, suspects):
+		clues=[]
 		for i in suspects :
-			clue.__init__(i, "Suspect")
-		pass
+			clues.append(Clue(i, "Suspect"))
+		return clues 
 		
-	def createWeaponsDeck(weapons):
-		pass
+	def createWeaponsDeck(self, weapons):
+		clues=[]
+		for i in weapons :
+			clues.append(Clue(i, "Weapon"))
+		return clues 
 		
-	def createRoomsDeck(rooms):
-		pass
+	def createRoomsDeck(self, rooms):
+		clues=[]
+		for i in rooms :
+			clues.append(Clue(i, "Room"))
+		return clues 
 		
-	def createMurder(suspectClues, roomClues, weaponClues):
-		pass
+	# Shuffle and use the individual decks to put together the murder to be solved.
+	def createMurder(self, suspectClues, roomClues, weaponClues):
+		murder = []
+		random.shuffle(suspectClues)
+		random.shuffle(roomClues)
+		random.shuffle(weaponClues)
+		murder.append(suspectClues.pop())
+		murder.append(roomClues.pop())
+		murder.append(weaponClues.pop())
+		self.murder = murder
+		
+	def getMurder(self):
+		return self.murder
+		
+	"""
+	Deal initial hands, and if a player leaves, place their hand in the "clueDeck" var
+	and pass it to remaining players.
+	"""
+	def dealHands(self, players, clueDeck):
+		numPlayers = len(players)
+		rounds = 0
+		while len(clueDeck) != 0 :
+			players[rounds%numPlayers].getHand().append(clueDeck.pop())
+			rounds += 1
 	
-	
+testPlayer1 = Player("test")
+testPlayer2 = Player("test2")
+Object = Game("id",[testPlayer1, testPlayer2])
+print("\n")
+thing=Object.getMurder()
+for i in testPlayer1.getHand():
+	print("Player 1 : " + i.getClueName())
+for i in testPlayer2.getHand():
+	print("Player 2 : " + i.getClueName())
--- src/server/map.py
+++ src/server/map.py
@@ -1,15 +1,17 @@
-class Map:
-    mapGrid = [][]
-    def __init__(self, players):
-        self.players = players
-		self.setupMap()
 
-    def movePlayer(self, player, move):
-        room = self.mapGrid[player.x][player.y]
-        self.checkRules(player, room, move)
 
-    def checkRules(player, room, move):
-        Rules.rule1()
+class Map :
+	def __init__(self, players) :
+		self.players = players
+		
+	def movePlayer(self, player, move) :
+		self.checkRules(player, room, move)
+		
 
-	def setupMap():
+	def checkRules(player, room, move) :
+		Rules.rule1()
+		
+
+	def setupMap() :
 		mapGrid.append("")
+		
