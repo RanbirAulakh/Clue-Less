dabc103fa9eb1c6e21c0ca2ebbda7e7817023bf0
toripalaoro <43078918+toripalaoro@users.noreply.github.com>
2020-04-16 22:36:43 -0400

    rules

--- src/server/rules.py
+++ src/server/rules.py
@@ -1,8 +1,12 @@
 class Rules:
 #Will fill out the rules as we set up more in the player and map classes to determine
 #what needs to be passed in to the rule methods.
-	def rule1():
-		return True
+	def moveValid(startRoom, endRoom):
+		connectionlist = startRoom.getConnections()
+		for connection in connectionlist):
+			if connection.name == endRoom.name:
+				return True
+		return False
 		
 	def rule2():
 		return False
