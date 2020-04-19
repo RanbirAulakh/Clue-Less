7e62513d840c4c162b6e39efd1224c37e502b4c2
Mike K <mikecknatz@gmail.com>
2020-04-19 11:35:00 -0400

    20200419: Stlye fixes, possible functions for move, guess, and accuse

--- ClueLess/game/logic/rules.py
+++ ClueLess/game/logic/rules.py
@@ -1,10 +1,10 @@
 class Rules:
 #Will fill out the rules as we set up more in the player and map classes to determine
 #what needs to be passed in to the rule methods.
-	def moveValid(startRoom, endRoom):
-		connectionlist = startRoom.getConnections()
-		for connection in connectionlist):
-			if connection.name == endRoom.name:
+	def move_valid(start_room, end_room):
+		connection_list = start_room.get_connections()
+		for connection in connection_list):
+			if connection.name == end_room.name:
 				return True
 		return False
 		
