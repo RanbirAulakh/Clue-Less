e30adf40a0c3151b95ba920d5549d909162a056d
Ranbir Aulakh <ranbir2010@live.com>
2020-04-20 01:03:02 -0400

    No idea whats changed? -- Accidently whitespaces?

--- ClueLess/game/forms.py
+++ ClueLess/game/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from game.models import Game
 
+
 class GameForm(forms.ModelForm):
     TYPE_CHOICES = (
         ('Public', 'Public'),
@@ -10,7 +11,6 @@ class GameForm(forms.ModelForm):
 
     name = forms.CharField(label="Lobby Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
     type = forms.ChoiceField(label="Lobby Type", choices=TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
-    # visibility = forms.BooleanField(label="Make it private", widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
     private_key = forms.CharField(label="Password", widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
 
     class Meta:
