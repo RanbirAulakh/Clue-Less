2038346325d889902cc50e8e1b03a4b357d11db3
Ranbir Aulakh <ranbir2010@live.com>
2020-04-21 00:41:18 -0400

    Fix indentation

--- ClueLess/game/logic/player.py
+++ ClueLess/game/logic/player.py
@@ -16,12 +16,13 @@ class Player:
 			lst.append(i.name)
 
 		return lst
-	def disprove(hand):
-		returnVal = [];
+
+	def disprove(self, hand):
+		returnVal = []
 		for card in self.hand:
-		    for proofcard in hand:
-			if card.get_clue_name == proofcard.get_clue_name:
-			    returnVal.add(card)     
+			for proofcard in hand:
+				if card.get_clue_name == proofcard.get_clue_name:
+					returnVal.add(card)
 		return returnVal
 	
 	def get_name(self):
